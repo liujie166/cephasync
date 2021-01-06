a649c203aded3d77bd0bab2f549a783c3aaea924
liujie <906756461@qq.com>
2021-01-06 21:48:23 +0800

    chunk add bufferptr for data buffer, correct some simple bug

--- rdma/Infiniband.cc
+++ rdma/Infiniband.cc
@@ -742,7 +742,7 @@ void Infiniband::MemoryManager::PoolAllocator::free(char * const block)
 {
   mem_info *m;
   Mutex::Locker l(lock);
-  size_t rx_buf_size = sizeof(Chunk) + manager->cct->_conf->ms_async_rdma_buffer_size;
+  size_t rx_buf_size = sizeof(Chunk) + g_ctx->manager->cct->_conf->ms_async_rdma_buffer_size;
   m = reinterpret_cast<mem_info *>(block);
   m->ctx->update_stats(-m->nbufs);
   Chunk *ch = m->chunks;
