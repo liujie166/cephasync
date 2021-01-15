f78398596a6b84dc02a4771aa24e14fd8168119b
liujie <906756461@qq.com>
2021-01-15 15:48:03 +0800

    [debug]

--- rdma/RDMAStack.cc
+++ rdma/RDMAStack.cc
@@ -256,7 +256,7 @@ void RDMADispatcher::polling()
          reg_window = (int)(reg_window*3/4 + 1);
        }*/
        int post_times = rx_ret/reg_window;
-       post_times = (post_times) ? post_time : 1;
+       post_times = (post_times) ? post_times : 1;
        post_backlog += post_times;
        for(; have_conn_buffer_empty && (post_backlog == 0); post_backlog--) {
          get_stack()->get_infiniband().post_chunks_to_srq(rx_ret, reg_window);
