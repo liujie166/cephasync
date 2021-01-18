560511bdf6bfc7fea104adb5ff1d6349faa2528f
liujie <906756461@qq.com>
2021-01-18 13:56:19 +0800

    overlap try1

--- rdma/RDMAStack.cc
+++ rdma/RDMAStack.cc
@@ -249,14 +249,15 @@ void RDMADispatcher::polling()
       polled.clear();
 
        post_backlog += rx_ret;
-      //if(post_backlog > threshold) {
+       int threshold = cct->_conf->ms_async_rdma_receive_queue_len;
+       if(post_backlog > threshold) {
         //uint64_t beg = Cycles::rdtsc();
         //auto record = post_backlog;
         post_backlog -= get_stack()->get_infiniband().post_chunks_to_srq(post_backlog);
         //uint64_t end = Cycles::rdtsc();
         //ldout(cct, 0) << __func__ << " malloc and register use " << Cycles::to_microseconds(end - beg, 0) << " us "
         //              << ", totally " << record << " chunks" << dendl;
-      //}
+        }
     }
 
     if (!tx_ret && !rx_ret) {
